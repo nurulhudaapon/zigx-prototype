26279f30d461bc251db832a125dd840e0c52213f
Nurul Huda (Apon) <me@nurulhudaapon.com>
2025-11-21 18:24:06 +0600

    feat(cli): version arg to upgrade cmd

--- src/cli/upgrade.zig
+++ src/cli/upgrade.zig
@@ -4,22 +4,35 @@ pub fn register(writer: *std.Io.Writer, reader: *std.Io.Reader, allocator: std.m
         .description = "Upgrade the version of ZX CLI",
     }, upgrade);
 
-    // try cmd.addFlag(version_flag);
+    try cmd.addFlag(version_flag);
 
     return cmd;
 }
 
 fn upgrade(ctx: zli.CommandContext) !void {
-    // const version = ctx.flag("version", []const u8);
-    // const version_str = if (std.mem.eql(u8, version, "latest")) "" else try std.fmt.allocPrint(ctx.allocator, "#v{s}", .{version});
-    // defer ctx.allocator.free(version_str);
+    const version = ctx.flag("version", []const u8);
 
-    // const fetch_uri = try std.fmt.allocPrint(ctx.allocator, "git+{s}{s}", .{ zx.info.repository, version_str });
-    // defer ctx.allocator.free(fetch_uri);
+    var maybe_cmd_str: ?[:0]u8 = null;
+    defer if (maybe_cmd_str) |s| ctx.allocator.free(s);
 
     const install_cmd = switch (builtin.os.tag) {
-        .windows => [_][:0]const u8{ "powershell", "-c", "irm ziex.dev/install.ps1 | iex" },
-        .linux, .macos => [_][:0]const u8{ "bash", "-c", "curl -fsSL https://ziex.dev/install | bash" },
+        .windows => blk: {
+            if (std.mem.eql(u8, version, "latest")) {
+                break :blk [_][:0]const u8{ "powershell", "-c", "irm ziex.dev/install.ps1 | iex" };
+            } else {
+                // TODO: support version for windows
+                break :blk [_][:0]const u8{ "powershell", "-c", "irm ziex.dev/install.ps1 | iex" };
+            }
+        },
+        .linux, .macos => blk: {
+            if (std.mem.eql(u8, version, "latest")) {
+                break :blk [_][:0]const u8{ "bash", "-c", "curl -fsSL https://ziex.dev/install | bash" };
+            } else {
+                const prefix = if (std.mem.startsWith(u8, version, "v")) "" else "v";
+                maybe_cmd_str = try std.fmt.allocPrintSentinel(ctx.allocator, "curl -fsSL https://ziex.dev/install | bash -s -- {s}{s}", .{ prefix, version }, 0);
+                break :blk [_][:0]const u8{ "bash", "-c", maybe_cmd_str.? };
+            }
+        },
         else => return error.UnsupportedOS,
     };
 
@@ -29,19 +42,19 @@ fn upgrade(ctx: zli.CommandContext) !void {
     const term = try system.wait();
     _ = term;
 
-    try ctx.writer.print("Version: ", .{});
+    try ctx.writer.print("Upgraded version to: ", .{});
     var zx_version = std.process.Child.init(&.{ "zx", "version" }, ctx.allocator);
     try zx_version.spawn();
     _ = try zx_version.wait();
 }
 
-// const version_flag = zli.Flag{
-//     .name = "version",
-//     .shortcut = "v",
-//     .description = "Version to update to",
-//     .type = .String,
-//     .default_value = .{ .String = "latest" },
-// };
+const version_flag = zli.Flag{
+    .name = "version",
+    .shortcut = "v",
+    .description = "Version to update to",
+    .type = .String,
+    .default_value = .{ .String = "latest" },
+};
 
 const std = @import("std");
 const zli = @import("zli");
